b5fa697a9ec1b8c18e797c9bd7ad6093d24d21a1
Erick Barrera <ebarreral.isc@gmail.com>
2021-11-30 18:08:08 -0600

    updates_grenrc

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,4 +1,7 @@
 # Changelog
 
-## v1.2.0 (30/11/2021)
-*No changelog for this release.*
+## v1.2.2 (30/11/2021)
+
+#### Uncategorised
+
+- [**closed**] Removes echo token [#5](https://github.com/erbalo/leetcode-problems/pull/5)
--- create_release_note.sh
+++ create_release_note.sh
@@ -2,11 +2,15 @@
 PARAM_PREVIOUS_VERSION=$1
 PARAM_CURRENT_VERSION=$2
 
+echo "PREV $PARAM_PREVIOUS_VERSION vs CUR $PARAM_CURRENT_VERSION"
+
 if [ -z $GREN_GITHUB_TOKEN ]; then
     echo "Access token should be an environment variable."
     echo "Check this doc: https://docs.github.com/en/authentication/keeping-your-account-and-data-secure/creating-a-personal-access-token"
     exit 1
 fi
 
+set -x
 gren changelog --generate --override --tags=$PARAM_PREVIOUS_VERSION..$PARAM_CURRENT_VERSION
 gren release --override --tags=$PARAM_PREVIOUS_VERSION..$PARAM_CURRENT_VERSION
+set +x
